60fbe8ba01170978b39055a255a6d0eaad9dc59d
lacey greene <laceylgreene@gmail.com>
2017-02-25 20:25:00 -0800

    this will hide the result answers until selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $("form#hawaii").submit(function(event){
 
-  var weather =$("input:radio[name=weather]:checked").val();
-  var weather =$("input:radio[name=landscape]:checked").val();
-  var weather =$("input:radio[name=event1]:checked").val();
-  var weather =$("input:radio[name=event2]:checked").val();
-  var weather =$("input:radio[name=children]:checked").val();
+  var weather = $("input:radio[name=weather]:checked").val();
+  var landscape = $("input:radio[name=landscape]:checked").val();
+  var event1 = $("input:radio[name=event1]:checked").val();
+  var event2 = $("input:radio[name=event2]:checked").val();
+  var children = $("input:radio[name=children]:checked").val();
 
   var result;
   if (weather === sun){
