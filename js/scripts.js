6eda5002a27a2a6f7d7a5e220255a1284963f344
lacey greene <laceylgreene@gmail.com>
2017-02-25 20:41:33 -0800

    troubleshoot

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
   $("form#hawaii").submit(function(event){
 
-  var weather = $("input:radio[name=weather]:checked").val();
-  var landscape = $("input:radio[name=landscape]:checked").val();
-  var event1 = $("input:radio[name=event1]:checked").val();
-  var event2 = $("input:radio[name=event2]:checked").val();
-  var children = $("input:radio[name=children]:checked").val();
+  var climate = $("input:radio[name=weather]:checked").val();
+  var view = $("input:radio[name=landscape]:checked").val();
+  var first = $("input:radio[name=event1]:checked").val();
+  var second = $("input:radio[name=event2]:checked").val();
+  var kids = $("input:radio[name=children]:checked").val();
 
-  var result;
-  if (weather === sun){
-    result === Maui;
+  if (climate === 'sun') {
+    $("#maui").show();
   }
 
-  $("#result").text(result);
   event.preventDefault();
-});
-
+  });
 )};
