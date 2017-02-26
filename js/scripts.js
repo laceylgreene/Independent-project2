31fd5a99e046f58d0fa8b9875e3a4f42b5adb0b6
lacey greene <laceylgreene@gmail.com>
2017-02-25 20:13:14 -0800

    this should set variables to each radio button question

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
   $("form#hawaii").submit(function(event){
 
   var weather =$("input:radio[name=weather]:checked").val();
+  var weather =$("input:radio[name=landscape]:checked").val();
+  var weather =$("input:radio[name=event1]:checked").val();
+  var weather =$("input:radio[name=event2]:checked").val();
+  var weather =$("input:radio[name=children]:checked").val();
 
   var result;
   if (weather === sun){
