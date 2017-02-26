a15a29595162255b60fa46f361020a772b64df85
lacey greene <laceylgreene@gmail.com>
2017-02-26 13:33:08 -0800

    this should have set the input variables needed to suggest the best island

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,15 @@ $(document).ready(function(){
   var second = $("input:radio[name=event2]:checked").val();
   var kids = $("input:radio[name=children]:checked").val();
 
-  if (climate === 'sun' && view === 'city' || 'boonies' && first === 'shop' && second === 'tour' || 'beach' && kids === 'yes' || 'no') {
-    $("#maui").show();
-
+  if (climate === 'rain' && view === 'boonies' && first === 'hike' && second === 'beach' && kids === 'yes' || 'no') {
+    $("#big").show();
   }
-
+  else if (climate === 'sun' && view === 'city' && first === 'shop' && second === 'club' && kids === 'no'){
+    $("#oahu").show();
+  }
+  else if (climate === 'sun' && view === 'city' || 'boonies' && first === 'shop' && second === 'club' || 'beach' && kids === 'yes' || 'no') {
+    $("#maui").show();
+  }
   event.preventDefault();
   });
 });
